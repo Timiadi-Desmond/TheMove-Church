b97f70671d9b97e5ba31cf3b38e62b3a789ecab5
Timiadi-Desmond <desmondtimiadi@gmail.com>
2026-08-05 11:17:44 +0100

    Initial commit

---
+++ Frontend projects/Campus/campus.js
@@ -0,0 +1,9 @@
+window.addEventListener("scroll", () => {
+    const nav = document.getElementById("navbar");
+
+    if(window.scrollY > 1){
+        nav.classList.add("scrolled");
+    }else{
+        nav.classList.remove("scrolled");
+    }
+});
