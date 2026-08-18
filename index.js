379fdf2ee81859031e0eff9283ae1131e91ac8a0
Timiadi-Desmond <desmondtimiadi@gmail.com>
2026-08-18 16:36:16 +0100

    Fix mobile menu layout

--- index.js
+++ index.js
@@ -1,3 +1,5 @@
+const sidemenu = document.getElementById('sidemenu')
+
 window.addEventListener('scroll', () =>{
     const navbar = document.getElementById('navbar')
     if(window.scrollY > 50){
@@ -7,3 +9,13 @@ window.addEventListener('scroll', () =>{
         navbar.classList.remove('scrolled')
     }
 })
+
+function closemenu(){
+    sidemenu.style.right = '-400px'
+}
+function openmenu(){
+    sidemenu.style.right = '0'
+}
+
+
+
